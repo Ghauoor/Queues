55da757896eee0f7c323807fbc10d53144dcb5ee
Ghayoor <ghayoorhussain701@gmail.com>
2023-03-09 00:00:37 +0500

    Custom Queue Using Array

---
+++ src/Basics/QueueY.java
@@ -0,0 +1,82 @@
+package Basics;
+
+public class QueueY {
+
+    public static void main(String args[]) {
+        Queue q = new Queue(5);
+        q.add(11);
+        q.add(12);
+        q.add(13);
+        q.printQue();
+
+
+//        System.out.println(q.remove());
+//        System.out.println(q.peek());
+    }
+
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear;
+
+
+        Queue(int size) {
+            this.size = size;
+            arr = new int[size];
+            rear = -1;
+        }
+
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+
+        public static boolean isFull() {
+            return rear == size - 1;
+        }
+
+
+        public static void add(int data) {
+            if (isFull()) {
+                System.out.println("Overflow");
+                return;
+            }
+
+
+            arr[++rear] = data;
+        }
+
+
+        //O(n)
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return arr[0];
+        }
+
+        public static void printQue() {
+            for (int i = 0; i < size; i++) {
+                System.out.println("Elements are "+arr[i]);
+            }
+        }
+    }
+}
+
+
