4fbabcbe54ea4481adbd3c63d1dea634edefd056
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-03-16 13:38:34 +0500

    Reverse a Queue using Recursion

--- src/Questions/Reverse_Queue.java
+++ src/Questions/Reverse_Queue.java
@@ -21,5 +21,25 @@ public class Reverse_Queue {
         return q;
     }
 
+    //________________Using Recur________________________
+
+    private Queue<Integer> rev(Queue<Integer> queue) {
+        //base case
+        if (queue.size() == 0) {
+            return queue;
+        }
+
+        int front = queue.peek();
+        queue.remove();
+
+        //rec
+        rev(queue);
+
+        //add again front ele
+        queue.add(front);
+
+        return queue;
+    }
+
 
 }
