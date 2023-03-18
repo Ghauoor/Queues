a21e80344f87264c40d849b18fa5b092537c1a78
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-03-18 05:12:36 +0500

    Find first negative number in every window of size K

--- src/Questions/First_Negative_Int_Every_window_Size_K.java
+++ src/Questions/First_Negative_Int_Every_window_Size_K.java
@@ -2,6 +2,7 @@ package Questions;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class First_Negative_Int_Every_window_Size_K {
     public static long[] printFirstNegativeInteger(long A[], int N, int K) {
@@ -40,4 +41,37 @@ public class First_Negative_Int_Every_window_Size_K {
         }
         return ans;
     }
+}
+
+class Compute {
+    public long[] printFirstNegativeInteger(long A[], int N, int K) {
+        int i = 0, j = 0;
+        Queue<Long> q = new LinkedList<>();
+        long result[] = new long[N - K + 1];
+        int idx = 0;
+        while (j < N) {
+            if (A[j] < 0) q.add(A[j]);
+
+            if (j - i + 1 == K) {
+
+                if (q.size() == 0) {
+                    result[idx++] = 0;
+
+                } else {
+
+                    result[idx++] = q.peek();
+                    if (A[i] == q.peek()) {
+                        q.poll();
+                    }
+                }
+
+                i++;
+            }
+
+            j++;
+        }
+        return result;
+
+    }
+
 }
